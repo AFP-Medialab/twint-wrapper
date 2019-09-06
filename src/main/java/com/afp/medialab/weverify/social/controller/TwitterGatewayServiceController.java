f91423a563d5523a1a53569e9a2619af5105c209
Bertrand Goupil <bertrand.goupil@allianz-assistance.com>
2019-09-06 17:22:21 +0200

    "add Logger and external msg"

--- src/main/java/com/afp/medialab/weverify/social/controller/TwitterGatewayServiceController.java
+++ src/main/java/com/afp/medialab/weverify/social/controller/TwitterGatewayServiceController.java
@@ -2,6 +2,9 @@ package com.afp.medialab.weverify.social.controller;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,13 +18,22 @@ import com.afp.medialab.weverify.social.model.Status;
 @RestController
 public class TwitterGatewayServiceController {
 	
+	private static Logger Logger = LoggerFactory.getLogger(TwitterGatewayServiceController.class);
+	
+	@Value("${application.home.msg}")
+	private String homeMsg;
+	
+	
 	@RequestMapping("/")
 	public @ResponseBody String home() {
-		return "service is up";
+		return homeMsg;
 	}
 
 	@RequestMapping(path = "/collect", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public @ResponseBody CollectResponse collect(@RequestBody CollectRequest collectRequest) {
+		Logger.info(collectRequest.getSearch());
+		Logger.info(collectRequest.getFrom().toString());
+		Logger.info(collectRequest.getUntil().toString());
 		String session = UUID.randomUUID().toString();
 		return new CollectResponse(session, Status.Done);
 
