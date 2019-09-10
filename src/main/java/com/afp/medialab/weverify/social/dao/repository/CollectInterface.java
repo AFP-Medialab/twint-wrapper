f302df995af1314df5c3d7a52cac876ce309f3e0
thibault <thibault.lhuillier@epita.fr>
2019-09-10 15:57:24 +0200

    Added @Transactional

--- src/main/java/com/afp/medialab/weverify/social/dao/repository/CollectInterface.java
+++ src/main/java/com/afp/medialab/weverify/social/dao/repository/CollectInterface.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 
 public interface CollectInterface extends JpaRepository<CollectHistory, Integer> {
@@ -14,14 +15,17 @@ public interface CollectInterface extends JpaRepository<CollectHistory, Integer>
     CollectHistory findCollectHistoryBySession(String session);
 
     @Modifying
+    @Transactional
     @Query("update CollectHistory collect set collect.status = :status where collect.session = :session")
     void updateCollectStatus(@Param("session") String session, @Param("status") Status status);
 
     @Modifying
+    @Transactional
     @Query("update CollectHistory collect set collect.processStart = :processStart where collect.session = :session")
     void updateCollectProcessStart(@Param("session") String session, @Param("processStart") Date processStart);
 
     @Modifying
+    @Transactional
     @Query("update CollectHistory collect set collect.processEnd = :processEnd where collect.session = :session")
     void updateCollectProcessEnd(@Param("session") String session, @Param("processEnd") Date processEnd);
 }
