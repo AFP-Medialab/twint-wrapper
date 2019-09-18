dbf30190b8ade8056a6e3414740b51ecd3145a03
juliette <juliette.casteran@epita.fr>
2019-09-18 10:24:37 +0200

    Search string now deterministe

--- src/main/java/com/afp/medialab/weverify/social/model/SearchModel.java
+++ src/main/java/com/afp/medialab/weverify/social/model/SearchModel.java
@@ -1,29 +1,55 @@
 package com.afp.medialab.weverify.social.model;
 
+import com.afp.medialab.weverify.social.twint.TwintThread;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchModel {
-    private String search;
-    private ArrayList<String> or;
-    private ArrayList<String> and;
-    private ArrayList<String> not;
 
-    public ArrayList<String> getOr() {
+    private static org.slf4j.Logger Logger = LoggerFactory.getLogger(TwintThread.class);
+    private String search;
+    private SortedSet<String> or;
+    private SortedSet<String> and;
+    private SortedSet<String> not;
+
+    public SearchModel(String search, SortedSet<String> or, SortedSet<String> and, SortedSet<String> not)
+    {
+        this.search = search.toLowerCase();
+        this.or = new TreeSet<>();
+        this.and = new TreeSet<>();
+        this.not = new TreeSet<>();
+        Logger.info("CONSTRUCTING");
+        if (or != null)
+            for (String str: or)
+                this.or.add(str.toLowerCase());
+
+        if (and != null)
+            for (String str: and)
+                this.and.add(str.toLowerCase());
+
+        if (not != null)
+            for (String str: not)
+                this.not.add(str.toLowerCase());
+    }
+
+    public SortedSet<String> getOr() {
         return or;
     }
 
-    public void setOr(ArrayList<String> or) {
+    public void setOr(SortedSet<String> or) {
         this.or = or;
     }
 
-    public ArrayList<String> getAnd() {
+    public SortedSet<String> getAnd() {
         return and;
     }
 
-    public void setAnd(ArrayList<String> and) {
+    public void setAnd(SortedSet<String> and) {
         this.and = and;
     }
 
@@ -35,11 +61,11 @@ public class SearchModel {
         this.search = search;
     }
 
-    public ArrayList<String> getNot() {
+    public SortedSet<String> getNot() {
         return not;
     }
 
-    public void setNot(ArrayList<String> not) {
+    public void setNot(SortedSet<String> not) {
         this.not = not;
     }
 }
